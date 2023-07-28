2dae6252e6d89de0d2646ff19f1fa43e602a4dec
drangar <drangar@us.ibm.com>
2023-07-28 10:17:21 -0700

    Update vxciaoktmj.py

--- vxciaoktmj/vxciaoktmj.py
+++ vxciaoktmj/vxciaoktmj.py
@@ -3,15 +3,18 @@ import os
 from faker import Faker
 
 # How many fake customer records do you want
-NUM_RECORDS = os.environ["num_records"]
+NUM_RECORDS = int(os.environ["num_records"])
 
 #Create faker object
 fake = Faker()
+f = open("demofile2.txt", "a")
 
 for x in range(NUM_RECORDS):
   #Print dummy data
-  print("Name:", fake.name())
-  print("Email:", fake.email())
-  print("Address:", fake.address())
-  print("Country:", fake.country())
-  print("URL:", fake.url())
+  f.write("Name:"+fake.name())
+  f.write("Email:"+fake.email())
+  f.write("Address:"+fake.address())
+  f.write("Country:"+fake.country())
+  f.write("URL:"+fake.url())
+  
+f.close()
