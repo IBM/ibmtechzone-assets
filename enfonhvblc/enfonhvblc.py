bc586d9e890738371acebcd9f2267e79e6f0867e
drangar <drangar@us.ibm.com>
2023-08-02 12:37:08 -0700

    Update enfonhvblc.py

--- enfonhvblc/enfonhvblc.py
+++ enfonhvblc/enfonhvblc.py
@@ -1,3 +1,4 @@
+import os
 from ibm_watson_machine_learning.foundation_models import Model
 from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes, DecodingMethods
