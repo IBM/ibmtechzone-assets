f986f9d5df282e8e99464142a56c47e73ac8b4cf
drangar <drangar@us.ibm.com>
2023-08-02 12:42:27 -0700

    Update enfonhvblc.py

--- enfonhvblc/enfonhvblc.py
+++ enfonhvblc/enfonhvblc.py
@@ -12,6 +12,7 @@ generate_params = {
 API_KEY = os.environ["apikey"]
 URL = os.environ["url"]
 PROJECT_ID = os.environ["project_id"]
+MYSQUAREROOT = os.environ['mysquareroot']
 
 model = Model(
     model_id=ModelTypes.FLAN_UL2,
@@ -23,6 +24,10 @@ model = Model(
     project_id=PROJECT_ID
     )
 
-q = "Premise:  At my age you will probably have learnt one lesson. Hypothesis:  It's not certain how many lessons you'll learn by your thirties. Does the premise entail the hypothesis?"
+if MYSQUAREROOT == 4 :
+    q = "Premise:  At my age you will probably have learnt one lesson. Hypothesis:  It's not certain how many lessons you'll learn by your thirties. Does the premise entail the hypothesis?"
+else :
+    q = "Answer the following question by reasoning step by step.  The cafeteria had 23 apples. If they used 20 for lunch, and bought 6 more, how many apple do they have?"
+    
 generated_response = model.generate(prompt=q)
 print(generated_response['results'][0]['generated_text'])
