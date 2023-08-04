fe4bd6314305cf0b882afa1a2ebba5a8a593b423
drangar <drangar@us.ibm.com>
2023-08-03 18:16:48 -0700

    Delete nrlkctaftg directory

--- nrlkctaftg/nrlkctaftg.py
+++
@@ -1,9 +0,0 @@
-import random
-NUM_SAMPLES = os.environ["num_samples"]
-def inside(p):
-    x, y = random.random(), random.random()
-    return x*x + y*y < 1
-
-count = sc.parallelize(range(0, NUM_SAMPLES)) \
-             .filter(inside).count()
-print("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))
