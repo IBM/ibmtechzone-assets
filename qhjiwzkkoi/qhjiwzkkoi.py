176fd860e199276c6a0f33a84b8f1158a5741adc
drangar <drangar@us.ibm.com>
2023-08-24 01:41:00 -0700

    updated summary stats

--- qhjiwzkkoi/qhjiwzkkoi.py
+++ qhjiwzkkoi/qhjiwzkkoi.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import pandas_profiling
-import streamlit as st
 from sqlalchemy import create_engine
-from streamlit_pandas_profiling import st_profile_report
 
 CONN_STRING = os.environ["conn_string"]
 TABLE_NAME = os.environ["table_name"]
@@ -11,6 +8,4 @@ query="SELECT * FROM "+TABLE_NAME
 my_conn = create_engine(CONN_STRING)
 
 df = pd.read_sql(query,my_conn)
-pr = df.profile_report()
-
-st_profile_report(pr)
+df.describe(include='all').transpose()
