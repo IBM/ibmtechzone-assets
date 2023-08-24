be4802aca26303589a667b8d299f2125a9a2a3f1
drangar <drangar@us.ibm.com>
2023-08-23 17:38:26 -0700

    update olm and remove test entry

--- jnimozvfyo/jnimozvfyo.sh
+++ jnimozvfyo/jnimozvfyo.sh
@@ -1,1 +1,2 @@
-oc logs -n openshift-operator-lifecycle-manager $(oc get pods -n openshift-operator-lifecycle-manager -lapp=catalog-operator -o name)
+mkdir data
+oc logs -n openshift-operator-lifecycle-manager $(oc get pods -n openshift-operator-lifecycle-manager -lapp=catalog-operator -o name)>data/olm-status.log
