f90fd88b78b23a2df0b294abbfdd7b6988075e27
demoframework <demoframework@ibm.com>
2023-08-24 07:39:49 +0000

    public asset commit by sandbox_username

---
+++ huwsmgcvcs/huwsmgcvcs.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Connect to Mysql database
+CONN_STRING = os.environ["conn_string"]
+my_conn = create_engine(CONN_STRING)
+
+# Create Pandas dataframe from file
+INPUT_DATA_FILE = os.environ["input_data_file"]
+csv_df=pd.read_csv(INPUT_DATA_FILE)
+
+# Save pandas data frame to MysqL table
+TABLE_NAME = os.environ["table_name"]
+csv_df.to_sql(TABLE_NAME, conn, if_exists='replace', index=False)
