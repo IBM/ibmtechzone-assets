f50d021894434466abc6a51646d4f683cad455ae
Girish Padmanabhan <pgirish@in.ibm.com>
2023-08-29 10:43:24 +0530

    Update almifbbhfb.sh

--- almifbbhfb/almifbbhfb.sh
+++ almifbbhfb/almifbbhfb.sh
@@ -7,7 +7,7 @@ sleep 1
 done
 
 cd "$app_name"
-python3 -m venv demoapp
-source demoapp/bin/activate
+#python3 -m venv demoapp
+#source demoapp/bin/activate
 pip install -r "$path_to_requirements_file"
 streamlit run "$path_to_requirements_main_file"
