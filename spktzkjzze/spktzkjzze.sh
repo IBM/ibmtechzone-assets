c042e9815ee31f7ed68ab143b264573cebf0471f
Girish Padmanabhan <pgirish@in.ibm.com>
2023-09-11 19:32:26 +0530

    patch for sqlite3 and avoid python venv

--- spktzkjzze/spktzkjzze.sh
+++ spktzkjzze/spktzkjzze.sh
@@ -7,7 +7,10 @@ sleep 1
 done
 
 cd "$app_name"
-python3 -m venv demoapp
-source demoapp/bin/activate
+#python3 -m venv demoapp
+#source demoapp/bin/activate
+sed -i "1i __import__('pysqlite3')" /home/user/.local/lib/python3.11/site-packages/chromadb/__init__.py
+sed -i “2i import sys" /home/user/.local/lib/python3.11/site-packages/chromadb/__init__.py
+sed -i “3i sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')" /home/user/.local/lib/python3.11/site-packages/chromadb/__init__.py
 pip install -r "$path_to_requirements_file"
 streamlit run "$path_to_requirements_main_file"
