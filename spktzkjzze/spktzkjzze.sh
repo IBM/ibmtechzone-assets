52021b4a14fef51af35d49863737137fd4e990cd
drangar <drangar@us.ibm.com>
2023-09-15 09:06:48 -0700

    check for chromadb

--- spktzkjzze/spktzkjzze.sh
+++ spktzkjzze/spktzkjzze.sh
@@ -10,11 +10,17 @@ cd "$app_name"
 #python3 -m venv demoapp
 #source demoapp/bin/activate
 pip install -r "$path_to_requirements_file"
-read -r firstline</home/user/.local/lib/python3.11/site-packages/chromadb/__init__.py
-if [ "$firstline" != "__import__('pysqlite3')" ]; 
-then
-    sed -i "1i __import__('pysqlite3')" /home/user/.local/lib/python3.11/site-packages/chromadb/__init__.py
-    sed -i "2i import sys" /home/user/.local/lib/python3.11/site-packages/chromadb/__init__.py
-    sed -i "3i sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')" /home/user/.local/lib/python3.11/site-packages/chromadb/__init__.py
+FILE_TO_CHECK=/home/user/.local/lib/python3.11/site-packages/chromadb/__init__.py
+
+if test -f "$FILE_TO_CHECK"; then
+    read -r firstline<$FILE_TO_CHECK
+    if [ "$firstline" != "__import__('pysqlite3')" ]; 
+    then
+        sed -i "1i __import__('pysqlite3')" $FILE_TO_CHECK
+        sed -i "2i import sys" $FILE_TO_CHECK
+        sed -i "3i sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')" $FILE_TO_CHECK
+    fi
+else
+    echo "No ChromaDB to fix"
 fi
 streamlit run "$path_to_requirements_main_file"
