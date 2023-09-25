4cc8c91079162d6560e74f2f4b67fb971e523258
demoframework <demoframework@ibm.com>
2023-09-25 02:21:56 +0000

    public asset commit by sandbox_username

---
+++ ekijremzvk/ekijremzvk.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+import boto3
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+#CORS(app)
+
+# load environment variable from .env file
+load_dotenv()
+
+service_endpoint=os.environ["COS_SERVICE_ENDPOINT"]
+cos_access_key_id=os.environ["COS_ACCESS_KEY"]
+cos_secret_access_key=os.environ["COS_SECRET_KEY"]
+
+cos_client = boto3.client('s3',
+                          endpoint_url = service_endpoint,
+                          aws_access_key_id=cos_access_key_id,
+                          aws_secret_access_key=cos_secret_access_key)
+
+print("service endpoint: ", service_endpoint)
+print("cos access key: ", cos_access_key_id)
+
+# Return all buckets in your COS instance
+def get_all_buckets(cos_client):
+    response = cos_client.list_buckets()
+    allbuckets = []
+    for bucket in response['Buckets']:
+        allbuckets.append(bucket['Name'])
+    return allbuckets
+
+@app.route('/buckets')
+def get_buckets():
+    # List all buckets in your COS instance
+    buckets = get_all_buckets(cos_client)
+    return jsonify(buckets)
+
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
