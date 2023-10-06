49724151dfe84a994fbec682f7dfb9065ef370f6
demoframework <demoframework@ibm.com>
2023-10-06 16:12:41 +0000

    public asset commit by sandbox_username

---
+++ meigqypeiz/meigqypeiz.sh
@@ -0,0 +1,9 @@
+git clone https://github.ibm.com/tech-garage-canada/watsonx-orion-demo.git
+cd watsonx-orion-demo
+cd backend
+npm install
+cd ../frontend
+npm install
+npm run start
+../backend
+npm run start
