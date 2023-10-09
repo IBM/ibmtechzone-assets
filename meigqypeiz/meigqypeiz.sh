6abbb7bde69d276a1cabc9bc30d899cfb28d4a85
demoframework <demoframework@ibm.com>
2023-10-09 12:53:17 +0000

    Delete folder meigqypeiz

--- meigqypeiz/meigqypeiz.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.ibm.com/tech-garage-canada/watsonx-orion-demo.git
-cd watsonx-orion-demo
-cd backend
-npm install
-cd ../frontend
-npm install
-npm run start
-../backend
-npm run start
