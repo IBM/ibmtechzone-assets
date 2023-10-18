14532c1d21dbebb52e475f94758344f38f0d6aa6
demoframework <demoframework@ibm.com>
2023-10-18 18:52:08 +0000

    public asset commit by sandbox_username

--- fstnmmugha/fstnmmugha.py
+++ fstnmmugha/fstnmmugha.py
@@ -4,6 +4,7 @@ ACCESS_KEY = os.environ["access_key"]
 SECRET_KEY = os.environ["secret_key"]
 BUCKET_NAME = os.environ["bucket_name"]
 S3_ENDPOINT = os.environ["s3_endpoint"]
+TARGET_BUCKET_NAME = os.environ["target_bucket_name"]
 
 s3_options = {
     'key': ACCESS_KEY,
@@ -13,4 +14,4 @@ s3_options = {
     }
 }
 df = dd.read_csv('s3://'+BUCKET_NAME+'/*.csv', storage_options=s3_options)
-df.compute().to_csv('combined.csv', index=False)
+df.compute().to_csv('s3://'+TARGET_BUCKET_NAME+'/combined.csv', index=False)
