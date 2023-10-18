04ff4267e238634a848eb1e788f427d46b2626fb
demoframework <demoframework@ibm.com>
2023-10-18 18:36:31 +0000

    public asset commit by sandbox_username

--- fstnmmugha/fstnmmugha.py
+++ fstnmmugha/fstnmmugha.py
@@ -2,6 +2,14 @@ import dask.dataframe as dd
 ACCESS_KEY = os.environ["access_key"]
 SECRET_KEY = os.environ["secret_key"]
 BUCKET_NAME = os.environ["bucket_name"]
+S3_ENDPOINT = os.environ["s3_endpoint"]
 
-df = dd.read_csv('s3://'+BUCKET_NAME+'/*.csv', storage_options={'key': ACCESS_KEY, 'secret': SECRET_KEY})
+s3_options = {
+    'key': ACCESS_KEY,
+    'secret': SECRET_KEY,
+    'client_kwargs': {
+        'endpoint_url': 'http://your-custom-endpoint:port'
+    }
+}
+df = dd.read_csv('s3://'+BUCKET_NAME+'/*.csv', storage_options=s3_options)
 df.compute().to_csv('combined.csv', index=False)
