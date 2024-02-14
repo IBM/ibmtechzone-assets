8a04485fb99333fa995cc83540f0256246c1d4b2
demoframework <demoframework@ibm.com>
2024-02-14 06:51:13 +0000

    public asset commit by sandbox_username

--- fzjegfohfw/fzjegfohfw.sh
+++ fzjegfohfw/fzjegfohfw.sh
@@ -3,7 +3,9 @@ apikey=$apikey
 concat_string=$username
 concat_string+=":"
 concat_string+=$apikey
-TOKEN=$concat_string|base64
+echo $concat_string
+TOKEN=$(echo $concat_string|base64)
+echo $TOKEN
 echo "[CP4D]">>cp4d_info.conf
-echo "TOKEN=$TOKEN">>cp4d_info.conf
+echo "TOKEN=${TOKEN}">>cp4d_info.conf
 cat cp4d_info.conf
