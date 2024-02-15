04f9bee2177b28385d6088e5a99b2157493a1adc
demoframework <demoframework@ibm.com>
2024-02-15 19:23:05 +0000

    public asset commit by sandbox_username

--- ouqqywwzce/ouqqywwzce.sh
+++ ouqqywwzce/ouqqywwzce.sh
@@ -3,12 +3,22 @@ cpdctl config profile use cpd
 echo "Current projects in the cluster"
 echo "==============================="
 cpdctl project list
-echo "Create new project for Data and AI called DataProject"
-echo "======================================================"
-project_uuid=$(uuidgen)
-STORAGE="{\"type\": \"assetfiles\", \"guid\": \"$project_uuid\"}"
-PROJECT_CREATION=cpdctl project create --name "DataProject" --output json --raw-output --generator cli --storage '{STORAGE_JSON}' -j 'location'
-echo $PROJECT_CREATION
+echo "Check if DataProject already exists"
+echo "===================================="
+DATAPROJECT_EXISTS=$(cpdctl project list --name="DataProject" --output json -j "(resources[].metadata.guid)[0]" --raw-output)
+if test "$DATAPROJECT_EXISTS" = "null"
+then
+    echo "DataProject does not exist so create it"
+    echo "Create new project for Data and AI called DataProject"
+    echo "======================================================"
+    project_uuid=$(cat /proc/sys/kernel/random/uuid)
+    STORAGE="{\"type\": \"assetfiles\", \"guid\": \"$project_uuid\"}"
+    PROJECT_CREATION=$(cpdctl project create --name "DataProject" --output json --raw-output --generator cli --storage "$STORAGE" -j 'location')
+else
+    echo "DataProject exists with Project UID=$DATAPROJECT_EXISTS"
+fi
 echo "Create new connections for Data and AI called DataProject"
-
+CREATE_CONN_PROPERTIES = "{\"database\": \"$database_name\",\"host\": \"$database_host\",\"password\": \"$database_password\",\"username\": \"$database_user\"}"
+CONNECTION_ID=$(cpdctl connection create --name "datasource-connection" --description "Connection to my datasource" --datasource-type $datasource_type --project-id $DATAPROJECT_EXISTS --properties $CREATE_CONN_PROPERTIES -j metadata.asset_id --origin-country us --output json -j 'metadata.asset_id')
+echo $CONNECTION_ID
 
