c5030612192d0762d435b2b165e7e7fe55356993
demoframework <demoframework@ibm.com>
2024-02-21 08:49:36 +0000

    public asset commit by sandbox_username

--- oppeeyfhom/oppeeyfhom.sh
+++ oppeeyfhom/oppeeyfhom.sh
@@ -7,9 +7,16 @@ cpdctl project list
 echo "Check if DataGovernance already exists"
 echo "===================================="
 project_id=$(cpdctl project list --name="DataGovernance" --output json -j "(resources[].metadata.guid)[0]" --raw-output)
-cpdctl project delete --project-id $project_id
+if test "$project_id" = "null"
+then
+    echo "Project does not exist"
+else
+    cpdctl project delete --project-id $project_id
+fi
+
 storage_json="{\"type\": \"assetfiles\", \"guid\": \"$project_uuid\"}"
-cpdctl project create --name "DataGovernance" --output json --raw-output --storage '$storage_json' --jmes-query 'location'
+echo $storage_json
+cpdctl project create --name "DataGovernance" --output json --raw-output --storage "$storage_json" --jmes-query 'location'
 cpdctl project list
 project_id=$(cpdctl project list --name="DataGovernance" --output json -j "(resources[].metadata.guid)[0]" --raw-output)
 cpdctl asset import start --project-id $project_id --import-file DataGovernance.zip --output json --jmes-query "metadata.id" --raw-output
