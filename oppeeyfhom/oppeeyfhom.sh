371239fd8ec6cee299721b357e8b47eec136d774
demoframework <demoframework@ibm.com>
2024-02-21 08:40:23 +0000

    public asset commit by sandbox_username

---
+++ oppeeyfhom/oppeeyfhom.sh
@@ -0,0 +1,13 @@
+project_uuid=$(cat /proc/sys/kernel/random/uuid)
+cpdctl config profile set cpd --url $cpd_url --username $cpd_username --apikey $cpd_apikey
+cpdctl config profile use cpd
+echo "Current projects in the cluster"
+echo "==============================="
+cpdctl project list
+echo "Check if DataGovernance already exists"
+echo "===================================="
+project_id=$(cpdctl project list --name="DataGovernance" --output json -j "(resources[].metadata.guid)[0]" --raw-output)
+cpdctl project delete --project-id $project_id
+storage_json="{\"type\": \"assetfiles\", \"guid\": \"$project_uuid\"}"
+cpdctl project create --name "DataGovernance" --output json --raw-output --storage '$storage_json' --jmes-query 'location'
+cpdctl project list
