c0b4f0c34538b2a49c7ff42ed2907d05fba7e03c
demoframework <demoframework@ibm.com>
2024-02-21 07:44:51 +0000

    public asset commit by sandbox_username

--- gzsgehnkir/gzsgehnkir.sh
+++ gzsgehnkir/gzsgehnkir.sh
@@ -3,7 +3,14 @@ cpdctl config profile use cpd
 echo "Current projects in the cluster"
 echo "==============================="
 cpdctl project list
+echo "Check if DataGovernance already exists"
+echo "===================================="
+project_id=$(cpdctl project list --name="DataGovernance" --output json -j "(resources[].metadata.guid)[0]" --raw-output)
+
 EXPORT_JSON = "{'all_assets': True}"
-result=$(cpdctl asset export start --project-id $project_id --assets '$EXPORT_JSON' --name demo-project-assets --output json --jmes-query "metadata.id" --raw-output)
-EXPORT_ID = result.s
+result=$(cpdctl asset export start --project-id $project_id --assets '$EXPORT_JSON' --name datagovernance-project-assets --output json --jmes-query "metadata.id" --raw-output)
+export_id = result.s
 echo 'Export ID='=$EXPORT_ID
+
+cpdctl asset export download --project-id $project_id --export-id $export_id --output-file project-assets.zip --progress
+
