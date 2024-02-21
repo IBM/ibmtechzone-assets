97163a4d39e3f3bd1e116756af98325b4a47bfd3
demoframework <demoframework@ibm.com>
2024-02-21 07:42:22 +0000

    public asset commit by sandbox_username

--- gzsgehnkir/gzsgehnkir.sh
+++ gzsgehnkir/gzsgehnkir.sh
@@ -1,3 +1,8 @@
+cpdctl config profile set cpd --url $cpd_url --username $cpd_username --apikey $cpd_apikey
+cpdctl config profile use cpd
+echo "Current projects in the cluster"
+echo "==============================="
+cpdctl project list
 EXPORT_JSON = "{'all_assets': True}"
 result=$(cpdctl asset export start --project-id $project_id --assets '$EXPORT_JSON' --name demo-project-assets --output json --jmes-query "metadata.id" --raw-output)
 EXPORT_ID = result.s
