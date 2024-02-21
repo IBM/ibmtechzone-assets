ae375a6706b2048b589dfe20543353091b4079e3
demoframework <demoframework@ibm.com>
2024-02-21 08:42:11 +0000

    public asset commit by sandbox_username

--- oppeeyfhom/oppeeyfhom.sh
+++ oppeeyfhom/oppeeyfhom.sh
@@ -11,3 +11,6 @@ cpdctl project delete --project-id $project_id
 storage_json="{\"type\": \"assetfiles\", \"guid\": \"$project_uuid\"}"
 cpdctl project create --name "DataGovernance" --output json --raw-output --storage '$storage_json' --jmes-query 'location'
 cpdctl project list
+project_id=$(cpdctl project list --name="DataGovernance" --output json -j "(resources[].metadata.guid)[0]" --raw-output)
+cpdctl asset import start --project-id $project_id --import-file DataGovernance.zip --output json --jmes-query "metadata.id" --raw-output
+
