e2747f6137132544331aba19f8b4d6a36bcba9d6
demoframework <demoframework@ibm.com>
2024-02-22 08:45:35 +0000

    public asset commit by sandbox_username

---
+++ djnyvugyop/djnyvugyop.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("demo").getOrCreate()
+df = spark.createDataFrame(
+    [
+        ("sue", 32),
+        ("li", 3),
+        ("bob", 75),
+        ("heo", 13),
+    ],
+    ["first_name", "age"],
+)
+df.show()
