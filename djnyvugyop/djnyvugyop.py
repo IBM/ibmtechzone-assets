dd03a25799e5f6a2bcada70b2acc4c535382f074
demoframework <demoframework@ibm.com>
2024-02-22 23:13:57 +0000

    public asset commit by sandbox_username

--- djnyvugyop/djnyvugyop.py
+++ djnyvugyop/djnyvugyop.py
@@ -1,13 +1,22 @@
-from pyspark.sql import SparkSession
+import pandas as pd
+import pyarrow.parquet as pq
+from io import StringIO
 
-spark = SparkSession.builder.appName("demo").getOrCreate()
-df = spark.createDataFrame(
-    [
-        ("sue", 32),
-        ("li", 3),
-        ("bob", 75),
-        ("heo", 13),
-    ],
-    ["first_name", "age"],
-)
-df.show()
+def determine_file_type(filename):
+    try:
+        with open(filename, 'rb') as f:
+            # Read the beginning of the file to check for Parquet magic bytes
+            magic_bytes = f.read(4)
+            f.seek(0)  # Reset file pointer to beginning
+            if magic_bytes == b'PAR1':
+                return 'Parquet'
+            else:
+                # Try to parse the content as CSV
+                pd.read_csv(filename)
+                return 'CSV'
+    except Exception as e:
+        return 'Unknown'
+
+filename=os.environ["file_to_load"]
+file_type = determine_file_type(filename)
+print(f"The file '{filename}' is of type: {file_type}")
