155c3174a0af686a603230698bb28c9c1f2fa245
demoframework <demoframework@ibm.com>
2024-02-23 06:45:11 +0000

    public asset commit by sandbox_username

---
+++ viffvqdsjw/viffvqdsjw.sh
@@ -0,0 +1,4 @@
+wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+tar -xvzf apache-maven-3.9.6-bin.tar.gz
+export PATH=/home/user/apache-maven-3.9.6/bin:$PATH
+mvn --version
