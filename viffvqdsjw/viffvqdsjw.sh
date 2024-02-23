eaebb56088db4535c5b7846db74854c568092bbc
demoframework <demoframework@ibm.com>
2024-02-23 07:30:43 +0000

    public asset commit by sandbox_username

--- viffvqdsjw/viffvqdsjw.sh
+++ viffvqdsjw/viffvqdsjw.sh
@@ -5,7 +5,16 @@ output_log="/projects/allScripts/test.log"
 input_json="/projects/allScripts/config.json"
 # Get the current timestamp
 timestamp=$(date +"%Y-%m-%d %T")
-welcome_message="Hello World"
+
+while read -r message
+do
+    printf '%s\n' "$message"
+done < "$input_json"
+
+welcome_message=${`echo "$message" | jq -r '.message'`}
+
+printf '%s\n' "$welcome_message"
+
 cd
 wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
 tar -xvzf apache-maven-3.9.6-bin.tar.gz
@@ -13,7 +22,7 @@ export PATH=/home/user/apache-maven-3.9.6/bin:$PATH
 mvn --version
 
 # Append the message to the log file
-echo "[$timestamp] $welcome_message --from bash" >> "$output_log"
+echo "[$timestamp] $welcome_message --from CLI" >> "$output_log"
 
 # Display the welcome message
 echo "Welcome Message: $welcome_message"
