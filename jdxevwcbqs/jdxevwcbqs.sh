e1fdb1f9f0e807daeea386aaf557872342ddea5f
demoframework <demoframework@ibm.com>
2024-03-21 18:48:16 +0000

    public asset commit by sandbox_username

--- jdxevwcbqs/jdxevwcbqs.sh
+++ jdxevwcbqs/jdxevwcbqs.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 wwpn="$1"
-
 if [[ $wwpn == *":"* ]]; then
     new_wwpn=$(echo "$wwpn" | sed 's/://g')
 else
     new_wwpn=$(echo "$wwpn" | sed 's/../&:/g; s/:$//')
 fi
-
 echo "$new_wwpn"
