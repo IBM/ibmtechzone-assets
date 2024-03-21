441006200f9b547cce8ab9eb32a047c03466316d
demoframework <demoframework@ibm.com>
2024-03-21 19:04:29 +0000

    public asset commit by sandbox_username

--- skyqocvisv/skyqocvisv.py
+++ skyqocvisv/skyqocvisv.py
@@ -1,14 +1,14 @@
-import sys
+import os
+
+wwpn = os.environ["wwpn"]
 
 def convert_wwpn(wwpn):
     if ":" in wwpn:
-        # Colons are present, so remove them
         new_wwpn = wwpn.replace(":", "")
     else:
-        # Colons are missing, so add them
         new_wwpn = ':'.join(wwpn[i:i+2] for i in range(0, len(wwpn), 2))
 
     return new_wwpn
-
+    
 new_wwpn = convert_wwpn(wwpn)
 print(new_wwpn)
