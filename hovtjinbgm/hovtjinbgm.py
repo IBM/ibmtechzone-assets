9e848e3bdf3f369fff752f9e162a992dbabf1fdf
demoframework <demoframework@ibm.com>
2024-04-03 19:36:12 +0000

    public asset commit by sandbox_username

--- hovtjinbgm/hovtjinbgm.py
+++ hovtjinbgm/hovtjinbgm.py
@@ -1,22 +1,29 @@
+import os
+os.system("pip3 install boto3")
 import prestodb
 import pandas as pd
-import os
-from pyspark.sql import SparkSession
+from io import StringIO
+import boto3
 
 #Read data from WatsonX.data using prestodb
 host_name = os.environ["host_name"]
+port = 443
 username = os.environ["username"]
 password = os.environ["password"]
 catalog_name = os.environ["catalog_name"]
 schema_name = os.environ["schema_name"]
+table_name = os.environ["table_name"]
+
+
 aws_access_key_id = os.environ["aws_access_key_id"]
 aws_secret_access_key = os.environ["aws_secret_access_key"]
-s3_bucket_path = os.environ["s3_bucket_path"]
+bucket_name = os.environ["bucket_name"]
+key = os.environ["key"]
 
 #Create the connection to prestodb
 conn=prestodb.dbapi.connect(
     host=host_name,
-    port=443,
+    port=port,
     user=username,
     password=password,
     http_scheme='https',
@@ -25,25 +32,28 @@ conn=prestodb.dbapi.connect(
     schema=schema_name,
 )
 cur = conn.cursor()
-
+sql_query = "select * from iceberg_data.iceberg_s3.{}".format(table_name)
 query = sql_query
+print(query)
 df = pd.read_sql_query(query, conn)
+print(df.head())
 
 
-# Create SparkSession
-spark = SparkSession.builder \
-    .appName("minio_to_s3") \
-    .getOrCreate()
 
-df_spark = spark.createDataFrame(df)
+csv_buffer = StringIO()
+df.to_csv(csv_buffer, index=False)
 
 # AWS credentials and S3 bucket path
 aws_access_key_id = aws_access_key_id
 aws_secret_access_key = aws_secret_access_key
 
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+s3.put_object(Bucket='wxdatatest',Key='asset_test.csv', Body=csv_buffer.getvalue())
+response = s3.get_object(Bucket='wxdatatest', Key='asset_test.csv')
+data = response['Body'].read().decode('utf-8')
 
-# Set AWS credentials
-spark.sparkContext._jsc.hadoopConfiguration().set('fs.s3a.access.key', aws_access_key_id)
-spark.sparkContext._jsc.hadoopConfiguration().set('fs.s3a.secret.key', aws_secret_access_key)
+# Read CSV data into a DataFrame
+df = pd.read_csv(StringIO(data))
 
-df_spark.write.mode("overwrite").csv((s3_bucket_path))
+# Display DataFrame
+print(df)
