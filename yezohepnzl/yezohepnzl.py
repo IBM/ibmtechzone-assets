7ed338abdd6c364b6027bf2ff1d7769c19597b08
demoframework <demoframework@ibm.com>
2024-04-16 09:42:24 +0000

    public asset commit by sandbox_username

--- yezohepnzl/yezohepnzl.py
+++ yezohepnzl/yezohepnzl.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import os
+MY_DATA_FILE_NAME = os.environ["my_data_file_name"]
 if os.path.isfile("/data/my_data_file.csv"):
     # If file exists read data from the data file and create data frame
-    df = pd.read_csv('/data/my_data_file.csv')
+    df = pd.read_csv(MY_DATA_FILE_NAME)
 else:
     # No data file exists so just generate some random data
     df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)),columns=list('ABCD'))
