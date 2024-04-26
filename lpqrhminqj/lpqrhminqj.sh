161da1d932ceb2958fc208f4199548742bb9de7b
demoframework <demoframework@ibm.com>
2024-04-26 14:48:53 +0000

    public asset commit by sandbox_username

--- lpqrhminqj/lpqrhminqj.sh
+++ lpqrhminqj/lpqrhminqj.sh
@@ -4,9 +4,9 @@ python3 -m venv venv
 source venv/bin/activate
 pip install git+https://github.com/instructlab/instructlab.git@stable
 ilab
-ilab download
 git clone https://github.com/instructlab/taxonomy
 ilab init
+ilab download
 ilab serve
 sleep 5
 ilab chat
