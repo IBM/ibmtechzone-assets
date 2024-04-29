9bcc94b33e4b3068d1247dea6263d6d53d356f55
demoframework <demoframework@ibm.com>
2024-04-29 07:39:35 +0000

    public asset commit by sandbox_username

---
+++ kypkujigoy/kypkujigoy.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+
+# selenium 3
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+def initialize_driver():
+    # Initialize the WebDriver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    return driver
+
+# Usage
+url=os.environ("url")
+driver = initialize_driver()
+driver.get(url)
