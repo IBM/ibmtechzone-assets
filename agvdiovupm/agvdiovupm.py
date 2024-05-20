a04b5738671f94486c90fd7f7709d18a8d5e71d7
demoframework <demoframework@ibm.com>
2024-05-20 02:35:58 +0000

    public asset commit by sandbox_username

---
+++ agvdiovupm/agvdiovupm.py
@@ -0,0 +1,25 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("API_KEY", None)
+collection_id = os.getenv("COLLECTION_ID", None)
+url = f"https://api.getpostman.com/collections/{collection_id}/transformations"
+
+headers = {
+    "Content-Type": "application/json",
+    "x-api-key": api_key
+}
+
+response = requests.get(url, headers=headers)
+
+# Check for successful response
+if response.status_code == 200:
+  # Assuming the response is JSON, parse it
+  data = response.json()
+  # Access the 'output' key and print it
+  output = data.get('output')
+  print(output)
+else:
+  print(f"Error: API request failed with status code {response.status_code}")
