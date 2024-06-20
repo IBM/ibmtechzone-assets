0e12ecb6a1bd100f59f474688d133816835118b0
demoframework <demoframework@ibm.com>
2024-06-20 11:28:32 +0000

    public asset commit by sandbox_username

--- dcovukpqkd/dcovukpqkd.py
+++ dcovukpqkd/dcovukpqkd.py
@@ -1,14 +1,13 @@
 import re
-import os
-java_file=os.getenv('java_file')
-java_file = f"""
-{java_file}
-"""
+import pandas as pd 
+from pprint import pprint
+
 
 def find_similar_lines_public(text):
     similar_lines = []
     inside_block = False
     pattern = re.compile(r'(private|public)\s+(\w+)\s+(\w+)\(.*?\)\s*{')
+    #pattern = re.compile(r'(private|public|protected)?\s+(\w+)\s+(\w+)(\(.*?\))?\s*{')
     lines = text.split('\n')
     for line in lines:
         if inside_block:
@@ -34,10 +33,30 @@ def find_similar_lines_private(text):
     return similar_lines
 
 
+variables_list = find_similar_lines_private(java_file)
+functions_list = find_similar_lines_public(java_file)
 
-similar_lines_1 = find_similar_lines_private(java_file)
-similar_lines_2 = find_similar_lines_public(java_file)
-for line in similar_lines_1:
-    print(line)
-for line in similar_lines_2:
-    print(line)
+
+private_vars = re.findall(r"private \w+ (\w+);", java_file)
+
+
+usage_dict = {var: [] for var in private_vars}
+
+
+method_pattern = re.compile(r"public \w+ (\w+)\(.*?\) \{([\s\S]*?)\}")
+
+methods = method_pattern.findall(java_file)
+
+
+for method, body in methods:
+    for var in private_vars:
+        if var in body:
+            usage_dict[var].append(method)
+
+data = [(var, ', '.join(funcs)) for var, funcs in usage_dict.items() if funcs]
+df = pd.DataFrame(data, columns=['Private Variable', 'Functions Using Variable'])
+
+
+pprint(variables_list)
+pprint(functions_list)
+pprint(df)
