b2190dc0934c92a76b6e88f7302f6df2a36d28e3
demoframework <demoframework@ibm.com>
2024-06-28 11:00:00 +0000

    public asset commit by sandbox_username

--- kdalcznvkf/kdalcznvkf.py
+++ kdalcznvkf/kdalcznvkf.py
@@ -39,8 +39,31 @@ def get_topic(conversations):
     print(model.get_topics())
     df=model.get_topic_info()
     df.to_csv("./topics_on_prompt_summary.csv")
+
+
+# Define the URL to download
+wget_url = os.environ["file_path"]
+# Create the 'data' directory if it doesn't exist
+os.makedirs('data', exist_ok=True)
+# Change the current working directory to 'data'
+os.chdir('data')
+# Get the current working directory
+CWD = os.getcwd()
+# Download the file from the URL
+response = requests.get(wget_url)
+file_name = wget_url.split('/')[-1]
+file_path = os.path.join(CWD, file_name)
+# Write the downloaded content to a file
+with open(file_path, 'wb') as file:
+    file.write(response.content)
+# Iterate through all files in the current working directory
+for file in os.listdir(CWD):
+    if file.endswith('.zip'):
+        # Unzip the file
+        with zipfile.ZipFile(file, 'r') as zip_ref:
+            zip_ref.extractall(CWD)
     
-filename=os.environ["filename"]
+filename="data/download"
 data=pd.read_csv(filename)
 data['summaries']=data['TITLE'] + data['ABSTRACT']
 conversations=data['summaries'].values.tolist()
